b7714151eecc455c7b803126e600d2a4153f97e3
Christophe Choffel <ulu@iq12.com>
2021-04-07 16:50:16 +0200

    qsd

---
+++ Runtime/Binders/Shader/IRISShaderBinderInt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.iris.common
+{
+	[AddComponentMenu("IRIS/Shaders/IRIS Shader int Binder")]
+	public class IRISShaderBinderInt : IRISShaderBinderBase
+	{
+		public FXDataProvider.INT_DATA_TYPE IntToBind;
+		public string IntPropertyName = "intparameter";
+		public bool ShowWarning = false;
+		void Update()
+		{
+			if (MyRenderer == null)
+				GetRenderer();
+
+			if (MyRenderer == null)
+				return;
+
+			float f = (float)FXDataProvider.GetInt(IntToBind);
+
+			ShowWarning = false;
+			if (!MyRenderer.sharedMaterial.HasProperty(IntPropertyName))
+			{
+				ShowWarning = true;
+			}
+			else
+			{
+				MyRenderer.sharedMaterial.SetFloat(IntPropertyName, f);
+			}
+
+		}
+	}
+}

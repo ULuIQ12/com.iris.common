a2acc7b2a59be710b12cccf848c5f7907f98b536
Christophe Choffel <ulu@iq12.com>
2021-04-07 14:12:46 +0200

    Create VFXIntBinder.cs

---
+++ Runtime/Binders/VFX/VFXIntBinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+
+
+namespace com.iris.common
+{
+	[AddComponentMenu("IRIS/Int Binder")]
+	[VFXBinder("IRIS/Int")]
+	public class VFXIntBinder: VFXBinderBase
+	{
+		[VFXPropertyBinding("Int"), SerializeField]
+		protected ExposedProperty IntProperty = "IntProperty";
+
+		public FXDataProvider.INT_DATA_TYPE IntToBind = FXDataProvider.INT_DATA_TYPE.UserCount;
+
+		public override bool IsValid(VisualEffect component)
+		{
+			return component.HasFloat(IntProperty);
+		}
+
+		public override void UpdateBinding(VisualEffect component)
+		{
+			component.SetInt(IntProperty, FXDataProvider.GetInt(IntToBind));
+		}
+
+		public override string ToString()
+		{
+			return "IRIS: Int Binder";
+		}
+
+	}
+}

92221e988c037542de945f1f8241704ec87d54d9
Christophe Choffel <ulu@iq12.com>
2021-05-04 14:19:48 +0200

    Update AvatarDepthPlatform.cs

--- Runtime/Utils/AvatarDepthPlatform.cs
+++ Runtime/Utils/AvatarDepthPlatform.cs
@@ -7,23 +7,23 @@ using com.rfilkov.components;
 namespace com.iris.common
 {
 	[RequireComponent(typeof(AvatarController))]
-    public class AvatarDepthPlatform : MonoBehaviour
-    {
+	public class AvatarDepthPlatform : MonoBehaviour
+	{
 		private Vector3 iosCamPosition = new Vector3(0.0f, 0.5f, 0f);
 		private Vector3 windowsCamPosition = new Vector3(0.0f, 1.0f, 0f);
 		private Vector3 iosEulerCamRot = new Vector3(-13.47f, 0.0f, 0f);
-		
+
 		public Transform camTransform;
 
-        void Start()
-        {
+		void Start()
+		{
 			AvatarController c = GetComponent<AvatarController>();
-			
+
 			if (c == null)
 				return;
 
 			//Debug.Log("appplaotf = " + Application.platform);
-			if( Application.platform == RuntimePlatform.IPhonePlayer)
+			if (Application.platform == RuntimePlatform.IPhonePlayer)
 			{
 				//c.flipLeftRight = false;
 				c.flipLeftRight = true;
@@ -38,8 +38,8 @@ namespace com.iris.common
 				camTransform.localPosition = windowsCamPosition;
 				camTransform.localRotation = Quaternion.identity;
 			}
-        }
-		
+		}
+
 		public void Update()
 		{
 			AvatarController c = GetComponent<AvatarController>();
@@ -47,7 +47,7 @@ namespace com.iris.common
 			if (c == null)
 				return;
 
-			Debug.Log("appplaotf = " + Application.platform);
+			//Debug.Log("appplaotf = " + Application.platform);
 			if (Application.platform == RuntimePlatform.IPhonePlayer)
 			{
 				c.flipLeftRight = true;
@@ -63,6 +63,6 @@ namespace com.iris.common
 				camTransform.localRotation = Quaternion.identity;
 			}
 		}
-		
+
 	}
 }

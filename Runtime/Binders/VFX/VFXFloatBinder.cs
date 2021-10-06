dfedd1ce0cddcba6e9bb2f398b563bb1e0a1329b
Christophe Choffel <ulu@iq12.com>
2021-10-06 11:51:20 +0200

    Update VFXFloatBinder.cs

--- Runtime/Binders/VFX/VFXFloatBinder.cs
+++ Runtime/Binders/VFX/VFXFloatBinder.cs
@@ -17,7 +17,8 @@ namespace com.iris.common
 
 		public FXDataProvider.FLOAT_DATA_TYPE FloatToBind = FXDataProvider.FLOAT_DATA_TYPE.AudioBeat;
 
-		public float Smoothing = 1f;
+		[Range(0.0f, 1.0f)]
+		public float Smoothing = 0f;
 		private float PreviousValue = 0f;
 
 		public override bool IsValid(VisualEffect component)
@@ -28,7 +29,7 @@ namespace com.iris.common
 		public override void UpdateBinding(VisualEffect component)
 		{
 			float Val = FXDataProvider.GetFloat(FloatToBind);
-			Val = Mathf.Lerp(PreviousValue, Val, Smoothing);
+			Val = Mathf.Lerp(PreviousValue, Val, 1f-Smoothing);
 			component.SetFloat(FloatProperty, Val );
 
 			PreviousValue = Val;
